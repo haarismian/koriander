3ff293c5968f5d7cbaf19bec1625069b5c4b6a10
Haaris Mian <mail@haarismian.com>
2021-07-28 01:19:34 -0400

    got all requests working

--- server/routes/api.js
+++ server/routes/api.js
@@ -21,4 +21,15 @@ router.route('/add').post((req, res) => {
     .catch((err) => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+router.route('/:id').delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json('User deleted.'))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
